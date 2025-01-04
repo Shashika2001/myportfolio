d9b866d51a783a056e09284fc4a32137657da7d8
Shashika2001 <ekanayakespm@gmail.com>
2025-01-04 17:00:42 +0530

    create pages and routes

--- myportfolio/src/components/Navbar/Navbar.jsx
+++ myportfolio/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,17 @@
-import './Navbar.css';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
   return (
-    <div className='navbar'>
-      Nav bar
+    <div className='navbar '> 
+
+      <div  className='bg-cyan-950 flex justify-between items-center max-w-6xl mx-auto p-3'>
+      <ul className="text-white flex gap-4">
+        <li>Dashboard</li>
+        <li>About me</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+      </div>
     </div>
   )
 }
