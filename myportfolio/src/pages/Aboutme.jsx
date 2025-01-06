d26a612bcc1a67d68db076dd5555485b06d68045
Shashika2001 <ekanayakespm@gmail.com>
2025-01-06 22:21:58 +0530

    add image to about me page

--- myportfolio/src/pages/Aboutme.jsx
+++ myportfolio/src/pages/Aboutme.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import image01 from '../assets/image01.jpg';
 
 export default function Aboutme() {
   return (
     <div>
       About me
+      <img src={image01} alt="Profile" />
     </div>
   )
 }
