304f9985c2e1295817823947f89780874adff55a
Shashika2001 <ekanayakespm@gmail.com>
2025-01-07 23:13:07 +0530

    add a small part to about me

--- myportfolio/src/pages/Aboutme.jsx
+++ myportfolio/src/pages/Aboutme.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 import image01 from '../assets/image01.jpg';
 
 export default function Aboutme() {
+  const imageStyle = {
+    width: '150px',        // Set the width of the image
+    height: '150px',       // Set the height of the image
+    borderRadius: '50%',   // This makes the image rounded
+    objectFit: 'cover',    // Ensures the image covers the area without distortion
+  };
   return (
     <div>
       About me
-      <img src={image01} alt="Profile" />
+      <img src={image01} alt="Profile" style={imageStyle}/>
     </div>
   )
 }
